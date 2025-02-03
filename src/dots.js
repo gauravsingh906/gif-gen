583b8b2efd82cd6fc5e3ee69a6610d3f3dc8b747
Gaurav Singh <125201478+gauravsingh906@users.noreply.github.com>
2025-02-03 20:46:35 +0530

    Update dots.js

--- src/dots.js
+++ src/dots.js
@@ -1,7 +1,7 @@
 const dots = Array.from({ length: 100 }, (_, i) => (
     <div
         key={i}
-        className="dot"
+        className="dot z--1"
         style={{
             left: `${Math.random() * 100}%`,
             top: `${Math.random() * 100}%`,
