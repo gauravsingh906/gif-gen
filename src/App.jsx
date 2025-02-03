8bb9b3819a874968ce6f01967c3ec4abd999bfbb
Gaurav Singh <125201478+gauravsingh906@users.noreply.github.com>
2025-02-03 20:49:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className=" background w-full h-screen z-20 overflow-y-auto flex flex-col relative items-center rounded-lg border border-black">
-      {dots}
+     
       <h1 className="bg-white rounded-lg w-11/12 text-center mt-[40px] 
       text-black font-bold text-4xl px-10 py-2">RANDOM GIFS</h1>
       <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
